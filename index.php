e0176f79e27d01c2aa72cf19389109800d991ec3
stalg94 <stalg28@gmail.com>
2021-10-07 23:02:59 +0300

    округление чисел

--- index.php
+++ index.php
@@ -7,35 +7,37 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-// пример неявного приведения типов, типы данных разные, но интерпритатор все понимает
-$str   ="45.2";
-$number = $str - 12;
-echo $number;
-echo "<br>";
+$tests = array(
+        "42",
+    1337,
+    0x539,
+    02471,
+    0b10100111001,
+    1337e0,
+    "0x539",
+        "02471",
+    "0b10100111001",
+    "not numeric",
+    array(),
+    9.1,
+    null
+);
 
-$var = 0.0;
-if ($var){
-    echo "Переменная $var рассматривается как true <br>";
+foreach ($tests as $element) {
+    if (is_numeric($element)){
+        echo var_export($element, true). " - arv", PHP_EOL . "<br>";
+    }    else  {
+        echo  var_export($element, true). " - ei ole arv", PHP_EOL . "<br>";
+    }
 }
-else   {
-    echo "Переменная $var рассматривается как false<br>";
-}
-//преобразование к другому типу
-$float = 4.3;
-$number = (int)$float;
-echo $number;
-echo "<br>";
+// округление
+echo round(21.23435478932, 2);
 
-$num=21;
-$f= (float)($num/2) - (int)($num/2);
-if($f){
-    echo "paaritu <br>";
-}else{
-    echo "paaris <br>";
-}
+echo ceil(4.3); //5
+echo ceil(9.999); //10
 
-
-
+echo floor(4.3); // 4
+echo floor (9.999); //9
 
 ?>
 </body>
