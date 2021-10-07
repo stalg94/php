e8925754512d2dfa4510a8c92755298eb9f6613b
stalg94 <stalg28@gmail.com>
2021-10-07 22:48:38 +0300

    приведение типов

--- index.php
+++ index.php
@@ -7,28 +7,32 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$user = "Ivan";
-
-if (isset($user)) //позволяет проверить, есть ли какое-то значение  у функции или нет
-    echo "Переменная существует <br>";
-else
-    echo "переменная отсутвует <br>";
-
-$str= "";
-if (empty($str)) // empty проверяет на пустоту какой-то переменной
-    echo "строка пустая <br>";
-else
-    echo "в строке чтото есть <br>";
-
-// как узнать тип переменных:
-echo gettype("wdf");
+// пример неявного приведения типов, типы данных разные, но интерпритатор все понимает
+$str   ="45.2";
+$number = $str - 12;
+echo $number;
 echo "<br>";
 
-//как узнать, число это или нет:
-if (is_int(12342))
-    echo "See on arv <br>";
-else
-    echo "ei ole arv <br>";
+$var = 0.0;
+if ($var){
+    echo "Переменная $var рассматривается как true <br>";
+}
+else   {
+    echo "Переменная $var рассматривается как false<br>";
+}
+//преобразование к другому типу
+$float = 4.3;
+$number = (int)$float;
+echo $number;
+echo "<br>";
+
+$num=21;
+$f= (float)($num/2) - (int)($num/2);
+if($f){
+    echo "paaritu <br>";
+}else{
+    echo "paaris <br>";
+}
 
 
 
