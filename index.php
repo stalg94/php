aa83e0d63a5969b134081560b327f86281e503d6
stalg94 <stalg28@gmail.com>
2021-10-07 23:43:43 +0300

    использование ООП

--- index.php
+++ index.php
@@ -22,6 +22,34 @@ echo $point2->x;
 unset($point2);
 //echo $point2->x;
 
+echo  Point::$num; // вывод заданной в классе постоянной переменной
+
+$first= new Point();
+$first->x=3;
+$first->y=3;
+
+$second= clone $first;
+$second->x=5;
+$second->y=5;
+
+echo "<br>";
+echo "x: {$first->x}, y: {$first->y}";
+echo "<br>";
+echo "x: {$second->x}, y: {$second->y}";
+
+pow(2,8);
+
+$p1= new Point();
+$p1->x=10;
+$p1->y=34;
+
+$p2= new Point();
+$p2->x=3;
+$p2->y=10;
+
+$distance=sqrt((pow(($p2->x-$p1->x),2))+(pow(($p2->y-$p1->y),2)));
+echo "<br>$distance";
+
 
 ?>
 </body>
