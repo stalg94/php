f180cf821ac470c063ed95f5e812256c66e8d4bb
stalg94 <stalg28@gmail.com>
2021-10-09 00:58:14 +0300

    1

--- index.php
+++ index.php
@@ -7,42 +7,27 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$char= "php";
-switch ($char) {
-    case "php":
-        echo "<h1>Язык PHP </h1>";
-        break;
-    case "js":
-        echo "<h1>Язык JavaScript </h1>";
-        break;
-    case "c#":
-        echo "<h1>Язык C# </h1>";
-        break;
-    default:
-        echo "<h1>Неизвестный язык!</h1>";
-}
+for ($i=0, $j=5; $i<5, $j>0; $i++,$j--){
+    echo "$i - $j<br>";
+    }
 echo "<br>";
-$number = 120;
-switch (true){
-    case ($number > 0 && $number <= 10):
-        echo "$number vähem kui 10 ja rohkem kui 0";
-        break;
-    case ($number > 10 && $number <= 100):
-        echo "$number vähem kui 100 ja rohkem kui 10";
-        break;
-    case ($number > 100 && $number <= 1000):
-        echo "$number vähem kui 1000 ja rohkem kui 100";
-        break;
-    default:
-        echo "Tundmatu arv";
-
+for ($y=5; $y>0; $y--){
+    echo "$y <br>";
 }
 
-
-
-
-
-
+echo "<br>";
+for ($a=2; $a<100; $a++){
+    for ($b = 2; $b<$a;$b++){
+        if (($a%$b)!= 0){
+            continue;
+        }else{
+            $flag = true;
+            break;
+        }
+    }
+    if (!$flag) echo "$a ";
+    $flag = false;
+}
 ?>
 </body>
 </html>
