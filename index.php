689f5acc0c0a2c258b3a880b570a0e612145e2f8
stalg94 <stalg28@gmail.com>
2021-10-09 00:31:33 +0300

    while

--- index.php
+++ index.php
@@ -7,41 +7,13 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$char= "php";
-switch ($char) {
-    case "php":
-        echo "<h1>Язык PHP </h1>";
-        break;
-    case "js":
-        echo "<h1>Язык JavaScript </h1>";
-        break;
-    case "c#":
-        echo "<h1>Язык C# </h1>";
-        break;
-    default:
-        echo "<h1>Неизвестный язык!</h1>";
+$i = 0;
+while ($i <=10){
+    $i++;
+    if ($i<4) continue;
+    if ($i>5) break;
+    echo "$i <br>";
 }
-echo "<br>";
-$number = 120;
-switch (true){
-    case ($number > 0 && $number <= 10):
-        echo "$number vähem kui 10 ja rohkem kui 0";
-        break;
-    case ($number > 10 && $number <= 100):
-        echo "$number vähem kui 100 ja rohkem kui 10";
-        break;
-    case ($number > 100 && $number <= 1000):
-        echo "$number vähem kui 1000 ja rohkem kui 100";
-        break;
-    default:
-        echo "Tundmatu arv";
-
-}
-
-
-
-
-
 
 ?>
 </body>
