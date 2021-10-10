5ed19d1593582ea41ebfdbd59c696b2adbef96df
stalg94 <stalg28@gmail.com>
2021-10-10 15:43:01 +0300

    zadania

--- index.php
+++ index.php
@@ -7,41 +7,22 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$char= "php";
-switch ($char) {
-    case "php":
-        echo "<h1>Язык PHP </h1>";
-        break;
-    case "js":
-        echo "<h1>Язык JavaScript </h1>";
-        break;
-    case "c#":
-        echo "<h1>Язык C# </h1>";
-        break;
-    default:
-        echo "<h1>Неизвестный язык!</h1>";
-}
-echo "<br>";
-$number = 120;
-switch (true){
-    case ($number > 0 && $number <= 10):
-        echo "$number vähem kui 10 ja rohkem kui 0";
-        break;
-    case ($number > 10 && $number <= 100):
-        echo "$number vähem kui 100 ja rohkem kui 10";
-        break;
-    case ($number > 100 && $number <= 1000):
-        echo "$number vähem kui 1000 ja rohkem kui 100";
-        break;
-    default:
-        echo "Tundmatu arv";
+ $array= ["red","green","blue","violet","gray"];
+ echo $array[rand(0,count($array)-1)];
 
-}
+ $lenth=rand(5,10);
+ for ($i=0;$i<$lenth;$i++){
+     $array[$i]=rand(0,100);
+ }
+ sort($array);
+echo "<pre>";
+ print_r($array);
+ echo "</pre>";
 
-
-
-
-
+ $month=file("month.txt");
+echo "<pre>";
+print_r($month);
+echo "</pre>";
 
 ?>
 </body>
