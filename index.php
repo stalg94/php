4a2054ec751311e8b8c8ad7d08c4d951e2ae85af
stalg94 <stalg28@gmail.com>
2021-10-10 15:30:36 +0300

    1

--- index.php
+++ index.php
@@ -7,41 +7,40 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$char= "php";
-switch ($char) {
-    case "php":
-        echo "<h1>Язык PHP </h1>";
-        break;
-    case "js":
-        echo "<h1>Язык JavaScript </h1>";
-        break;
-    case "c#":
-        echo "<h1>Язык C# </h1>";
-        break;
-    default:
-        echo "<h1>Неизвестный язык!</h1>";
+$arr= [5=> 1,2,3];
+for ($i=0; $i<10; $i++){
+    if (isset($arr[$i])){
+        echo "Element \$arr[$i] olemas<br>";
+    }else{
+        echo "Element \$arr[$i] puudu<br>";
+    }
 }
+
+if (in_array(2,$arr)){
+    echo "Arv 2 on leitud massivis arr<br>";
+}else{
+    echo "arv 2 on puudu<br>";
+}
+
+if (array_key_exists(5,$arr)){
+    echo "Key 5 on leitud massivis arr<br>";
+}else{
+    echo "Key 5 on puudu<br>";
+}
+if (array_key_exists(5,$arr)){
+    echo "Key 5 on leitud massivis arr<br>";
+}else{
+    echo "Key 5 on puudu<br>";
+}
+
+echo array_search(2,$arr);
 echo "<br>";
-$number = 120;
-switch (true){
-    case ($number > 0 && $number <= 10):
-        echo "$number vähem kui 10 ja rohkem kui 0";
-        break;
-    case ($number > 10 && $number <= 100):
-        echo "$number vähem kui 100 ja rohkem kui 10";
-        break;
-    case ($number > 100 && $number <= 1000):
-        echo "$number vähem kui 1000 ja rohkem kui 100";
-        break;
-    default:
-        echo "Tundmatu arv";
+echo array_search(5,$arr);
 
-}
-
-
-
-
-
+unset($arr[6]);
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
 
 ?>
 </body>
