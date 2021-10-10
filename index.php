cad6882a7fdda1f8a1793c6b045fed0513273c85
stalg94 <stalg28@gmail.com>
2021-10-10 16:58:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,36 +7,46 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$char= "php";
-switch ($char) {
-    case "php":
-        echo "<h1>Язык PHP </h1>";
-        break;
-    case "js":
-        echo "<h1>Язык JavaScript </h1>";
-        break;
-    case "c#":
-        echo "<h1>Язык C# </h1>";
-        break;
-    default:
-        echo "<h1>Неизвестный язык!</h1>";
+function sum(int $a,int $b): int {
+    return $a+$b;
 }
-echo "<br>";
-$number = 120;
-switch (true){
-    case ($number > 0 && $number <= 10):
-        echo "$number vähem kui 10 ja rohkem kui 0";
-        break;
-    case ($number > 10 && $number <= 100):
-        echo "$number vähem kui 100 ja rohkem kui 10";
-        break;
-    case ($number > 100 && $number <= 1000):
-        echo "$number vähem kui 1000 ja rohkem kui 100";
-        break;
-    default:
-        echo "Tundmatu arv";
 
+echo sum(2,3);
+
+function outArguments(...$items){//аргумент items рассматривается как массив
+    foreach ($items as $arg){
+        echo "$arg<br>";
+    }
 }
+outArguments("PHP","JS","C++","Python");
+
+function outArgument($a,$b,$c,$d){
+    echo "$a<br>";
+    echo "$b<br>";
+    echo "$c<br>";
+    echo "$d<br>";
+}
+$item  = ["PHP","JS","C++","Python"];
+outArgument(...$item);
+
+function sums(){
+    global $var;
+    $var=2;
+    return $var;
+}
+
+$var = 6;
+echo sums();
+echo "<br>$var<br>";
+
+function statics(){
+    static $count=0; //за счет static данная переменная помнит свое предыдущее значение
+    return ++$count;
+}
+echo statics()."<br>";
+echo statics()."<br>";
+echo statics()."<br>";
+echo statics()."<br>";
 
 
 
