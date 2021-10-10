451361e0f515f307219836ad65df56bc92e85873
stalg94 <stalg28@gmail.com>
2021-10-10 14:25:55 +0300

    1

--- index.php
+++ index.php
@@ -7,36 +7,32 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$char= "php";
-switch ($char) {
-    case "php":
-        echo "<h1>Язык PHP </h1>";
-        break;
-    case "js":
-        echo "<h1>Язык JavaScript </h1>";
-        break;
-    case "c#":
-        echo "<h1>Язык C# </h1>";
-        break;
-    default:
-        echo "<h1>Неизвестный язык!</h1>";
-}
-echo "<br>";
-$number = 120;
-switch (true){
-    case ($number > 0 && $number <= 10):
-        echo "$number vähem kui 10 ja rohkem kui 0";
-        break;
-    case ($number > 10 && $number <= 100):
-        echo "$number vähem kui 100 ja rohkem kui 10";
-        break;
-    case ($number > 100 && $number <= 1000):
-        echo "$number vähem kui 1000 ja rohkem kui 100";
-        break;
-    default:
-        echo "Tundmatu arv";
+$arr=array("Hello","world","!"); //первый способ
+echo $arr[1];
 
-}
+echo "<pre>"; //сохраняет абзацы и форматирование
+print_r($arr); // вывод массив на экран
+echo "</pre>";
+
+$arr1=[5=>"Hello","world",8=>"!"]; //второй способ
+
+echo "<pre>"; //сохраняет абзацы и форматирование
+print_r($arr1); // вывод массив на экран
+echo "</pre>";
+
+$var= "Hello World!"; // третий способ
+$arr2 = (array) $var;
+echo "<pre>"; //сохраняет абзацы и форматирование
+print_r($arr2); // вывод массив на экран
+echo "</pre>";
+
+$arr3 = ["one" =>"1", "two"=>"2"];
+echo "<pre>";
+print_r($arr3);
+echo "</pre>";
+echo $arr3["one"];
+
+
 
 
 
