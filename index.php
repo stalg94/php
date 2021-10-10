776af2bcc7794ae1a95e97f887111abc8a1e26eb
stalg94 <stalg28@gmail.com>
2021-10-10 15:03:33 +0300

    1

--- index.php
+++ index.php
@@ -7,42 +7,30 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$char= "php";
-switch ($char) {
-    case "php":
-        echo "<h1>Язык PHP </h1>";
-        break;
-    case "js":
-        echo "<h1>Язык JavaScript </h1>";
-        break;
-    case "c#":
-        echo "<h1>Язык C# </h1>";
-        break;
-    default:
-        echo "<h1>Неизвестный язык!</h1>";
+$number= ["1","2","3","4","hello",true,54];
+for ($i=0; $i<count($number);$i++)
+    echo "$number[$i]<br>";
+
+$arr=[
+        "auto"=>"Dodge",
+    "plane"=>"IL-2",
+    "ship"=>"Black Paerl"
+];
+foreach ($arr as $key => $value){
+    echo "$key : $value <br>";
+};
+
+$transport = [
+    "Auto"=>["BMW","Audi","Saab"],
+    "Lennukid"=>["Boeing","IL","Airbus"],
+    "Laevad"=>["Parv","Laev","AllveeLaev"]
+];
+foreach ($transport as $key => $array){
+    echo "<b>$key</b><br>";
+    foreach ($array as $value){
+        echo "<li>$value</li>";
+    }
 }
-echo "<br>";
-$number = 120;
-switch (true){
-    case ($number > 0 && $number <= 10):
-        echo "$number vähem kui 10 ja rohkem kui 0";
-        break;
-    case ($number > 10 && $number <= 100):
-        echo "$number vähem kui 100 ja rohkem kui 10";
-        break;
-    case ($number > 100 && $number <= 1000):
-        echo "$number vähem kui 1000 ja rohkem kui 100";
-        break;
-    default:
-        echo "Tundmatu arv";
-
-}
-
-
-
-
-
-
 ?>
 </body>
 </html>
