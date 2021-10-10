ec28a2ace2e0baf72e17a6396f337680e0082fd1
stalg94 <stalg28@gmail.com>
2021-10-10 17:53:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,38 +7,15 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$char= "php";
-switch ($char) {
-    case "php":
-        echo "<h1>Язык PHP </h1>";
-        break;
-    case "js":
-        echo "<h1>Язык JavaScript </h1>";
-        break;
-    case "c#":
-        echo "<h1>Язык C# </h1>";
-        break;
-    default:
-        echo "<h1>Неизвестный язык!</h1>";
+$str = "Hello world";
+echo $str[0]."<br>";
+$str1 = "Привет мир!";
+echo mb_strlen($str1)."<br>"; // подсчет количества символов
+// вывод строки посимвольно
+for ($i=0; $i < strlen($str); $i++){
+    echo $str[$i]."<br>";
 }
-echo "<br>";
-$number = 120;
-switch (true){
-    case ($number > 0 && $number <= 10):
-        echo "$number vähem kui 10 ja rohkem kui 0";
-        break;
-    case ($number > 10 && $number <= 100):
-        echo "$number vähem kui 100 ja rohkem kui 10";
-        break;
-    case ($number > 100 && $number <= 1000):
-        echo "$number vähem kui 1000 ja rohkem kui 100";
-        break;
-    default:
-        echo "Tundmatu arv";
-
-}
-
-
+chr(36)
 
 
 
