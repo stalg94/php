f2b51d587d0dd8db59fb9e77bb6f3ceca484f7ae
stalg94 <stalg28@gmail.com>
2021-10-12 19:05:48 +0300

    1

--- index.php
+++ index.php
@@ -6,19 +6,37 @@
     <title><?="Uus leht"; ?></title>
 </head>  <!-- comment ctrl+/ -->
 <body>
-<form method="get">
-    <input type="text" name="first"><br>
-    <input type="text" name="second"><br>
-    <input type="submit" value="Отправить"><br>
+<form method="post" action="checkbox.php">
+    <input type="checkbox" name="html" value="1" checked>Я знаком с HTML<br>
+    <input type="checkbox" name="css" value="2">Я знаком с CSS<br>
+    <input type="checkbox" name="js" value="3">Я знаком с JS<br>
+    <input type="checkbox" name="php" value="4">Я знаком с PHP<br>
+    <input type="submit" value="Отправить">
+</form>
+<form method="post" action="select.php">
+    <select name="fst[]" multiple size="3">
+        <option value="1" selected>Esimene rida</option>
+        <option value="2" >Teine rida</option>
+        <option value="3" >Kolmas rida</option>
+    </select>
+    <br>
+    <select name="snd">
+        <option value="one" >Esimene rida</option>
+        <option value="two" >Teine rida</option>
+        <option value="three" >Kolmas rida</option>
+    </select>
+        <input type="submit" value="Отправить">
+</form>
+<form method="post" action="radio.php">
+    <input type="radio" name="mark" value="1">1<br>
+    <input type="radio" name="mark" value="2">2<br>
+    <input type="radio" name="mark" value="3">3<br>
+    <input type="radio" name="mark" value="4" checked>4<br>
+    <input type="radio" name="mark" value="5">5<br>
+    <input type="submit" value="Отправить">
 </form>
 <?php
-if (empty($_GET["first"])&& empty($_GET["first"])){
-    exit("Текстовые поля не заполнены");
-}else{
-    echo "<pre>";
-    print_r($_GET);
-    echo "</pre>";
-}
+
 
 ?>
 </body>
