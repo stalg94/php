37041b750c43b153439f5279c69e472e6731f7b4
stalg94 <stalg28@gmail.com>
2021-10-12 21:25:59 +0300

    1

--- index.php
+++ index.php
@@ -7,19 +7,63 @@
     <link rel="stylesheet" href="style.css">
 </head>  <!-- comment ctrl+/ -->
 <body>
+<?php
+//class Hello {
+//    public  function printText(){
+//        return "Hello world!";
+//    }
+//}
+//$obj= new Hello();
+//echo $obj->printText();
 
-<form method="post" action="sendmail.php">
-    <select name="subject">
-        <option disabled selected>Тема письма</option>
-        <option value="1">Вопрос по уроку</option>
-        <option value="2">Личный вопрос</option>
-        <option value="3">Благодарнсть</option>
-    </select>
-    <input type="email" name="email" placeholder="Введите Ваш email" maxlength="50" required >
-    <textarea name="message" placeholder="Введите сообщение" maxlength="150" required></textarea>
-    <img src="captcha1.png">
-    <input type="number" name="captcha" placeholder="Введите ответ" maxlength="3" required>
-    <input type="submit" value="Отправить письмо">
-</form>
+//class Point{
+//    private  $x;
+//    private  $y;
+//    public function setX($x){
+//        $this->x = $x;
+//    }
+//    public function setY($y){
+//        $this->y = $y;
+//    }
+//    public function getX(){
+//        return  $this->x;
+//    }
+//    public function getY(){
+//        return  $this->y;
+//    }
+//    public function distance(){
+//        return sqrt($this->getX()**2 + $this->getY()**2);
+//    }
+//}
+//$p1= new Point();
+//$p1->setX(5);
+//$p1->setY(7);
+//
+//echo $p1->distance();
+//class Hello {
+//    public static function printText(){
+//        return "Hello world!";
+//    }
+//}
+//echo Hello::printText(); //если статик, то переменная для вызова объекта класса не нужна
+
+class Page{
+    static $content = "Тело сайта<br>";
+    public static function footer(){
+        return "Это подвал сайта<br>";
+    }
+    public static function header(){
+        return "Это шапка сайта<br>";
+    }
+    public static function site(){
+        echo self::header().//self используется чтобы сослаться на статическую переменную этого же класса
+            self::$content.
+            self::footer();
+    }
+}
+Page::site();
+
+?>
+
 </body>
 </html>
