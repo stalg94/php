0e2a673135b7e676583b2351d15675e8deaaa6c4
stalg94 <stalg28@gmail.com>
2021-10-12 22:17:30 +0300

    1

--- index.php
+++ index.php
@@ -8,14 +8,18 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-class Animal {
+abstract class Animal{ //мы не можем создавать объекты данного класса, но можем наследоваться
     public  $legs = 4;
-    public  function info(){
+    public  function info(){ //не можем переопредляет метод final
         echo "У меня {$this->legs} лапы.";
     }
+    abstract public function color();
 }
 
 class Dog extends  Animal {
+    public function color(){
+        echo "red";
+    }
     public $name = "Собака";
     public  function info(){ //переопределяем метод, называется перегрузкой
         echo "Я собака. У меня {$this->legs} лапы.";
@@ -23,12 +27,18 @@ class Dog extends  Animal {
     public function voice(){ //protected недоступен извне класса, но доступен при наследовании
         echo "{$this->name} издает звук гав-гав.";
     }
+    public function parentInfo(){
+        parent::info();
+    }
 }
 $dog = new Dog();
 $dog->info();
 $dog->voice();
 
 class Cat extends  Animal {
+    public function color(){
+        echo "black";
+    }
     public $name = "Кошка";
     public function voice(){
         echo "{$this->name} издает звук мяу-мяу.";
@@ -38,8 +48,22 @@ $cat = new Cat();
 $cat->info();
 $cat->voice();
 
+echo "<br>";
 
-
+$dog->parentInfo();
+echo "<br>";
+if ($dog instanceof Animal){//принадлежность классу
+    echo "Dog является экземпляром класса Animal";
+}
+if ($dog instanceof Dog){
+    echo "Dog является экземпляром класса Dog";
+}
+if ($cat instanceof Cat){
+    echo "Cat является экземпляром класса Cat";
+}
+if ($cat instanceof Animal){//принадлежность классу
+    echo "Cat является экземпляром класса Animal";
+}
 
 
 ?>
