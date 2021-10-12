63e8cb05d307dfc864048478d28206c397a20a79
stalg94 <stalg28@gmail.com>
2021-10-12 11:27:40 +0300

    1

--- index.php
+++ index.php
@@ -7,19 +7,19 @@
 </head>  <!-- comment ctrl+/ -->
 <body>
 <?php
-$arr = [
-        "name"=>"Petr",
-    "phones"=>[
-            "888",
-        "777"
-    ]
-];
-$encodeArr = json_encode($arr);
-echo $encodeArr;
 echo "<pre>";
-print_r(json_decode($encodeArr,true));
+print_r($_GET);
 echo "</pre>";
 
+echo "<a href='test.php?phrase=".urlencode("Привет, мир!")."'>Ссылка</a>";
+
+$url= "http://user:pass@www.site.ru/path/index.php?par=value#anch";
+echo "<pre>";
+print_r(parse_url($url));
+echo "</pre>";
+
+/*при использовании GET данные (чаще значение параметров) прикрепляются к URI ресурса.
+Если в отображаемом URL страницы в конце присутствует знак вопроса и затем идет перечисление пар «параметр-значение», разделенных знаком амперсанда, то используется GET.*/
 
 ?>
 </body>
