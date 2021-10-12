d3ee9e8f4857ed96ef2edba7dc1d689187da4522
stalg94 <stalg28@gmail.com>
2021-10-12 11:04:25 +0300

    ?
    надо смотреть видео про JSON и Javascript

--- index.php
+++ index.php
@@ -6,32 +6,19 @@
     <title><?="Uus leht"; ?></title>
 </head>  <!-- comment ctrl+/ -->
 <body>
-<form action="handler.php" method="post">
-    Сообщение: <br>
-    <textarea name="msg" cols="50" rows="5"></textarea><br>
-    <input type="submit" name="Lisada">
-</form>
 <?php
-$str= "<p>Обычный текст</p><br>
-<br>Жирный текст</br>";
-echo htmlspecialchars(strip_tags($str));//удаляет все html теги, чтобы был обычный текст
-printf("Первое число - %d",26);
-echo "<br>";
-$red=255;
-$green=255;
-$blue= 100;
-printf("#%X%X%X",$red,$green,$blue);
-echo "<br>";
-$dtr="Имя, Фамилия, e-mail";
+$arr = [
+        "name"=>"Petr",
+    "phones"=>[
+            "888",
+        "777"
+    ]
+];
+$encodeArr = json_encode($arr);
+echo $encodeArr;
 echo "<pre>";
-print_r(explode(", ",$dtr,3));//разбивает стрку на массив с определенным разделителем и кол-ом элементов (3)
-echo "</pre";
-$arr=["Имя","Фамилия","e-mail","Номер"];
-echo implode(", ",$arr); //делаем из массива строку
-
-
-
-
+print_r(json_decode($encodeArr,true));
+echo "</pre>";
 
 
 ?>
